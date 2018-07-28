3cf94aaaf1356d03b64a1dc7ee69de522ad50435
tomoaki-kimura <tkim19750404@gmail.com>
2018-07-28 09:38:35 +0900

    test start

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -40,6 +40,9 @@ class UsersController < ApplicationController
   # PATCH/PUT /users/1
   # PATCH/PUT /users/1.json
   def update
+    p '***********'
+    p @user.image
+    p @user.image.original_filename
     respond_to do |format|
       if @user.update(user_params)
         format.html { redirect_to @user, notice: 'User was successfully updated.' }
